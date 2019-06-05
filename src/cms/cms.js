bbbdc382eca1c72b6941eab5f8b0f107f74c2702
Carlos Gonzalez <homearanya@gmail.com>
2019-06-05 10:45:50 +0200

    new customRelation Widget to fix unsaved issue

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,10 @@ import CMS from 'netlify-cms';
 import CategoryPreview from './preview-templates/CategoryPreview';
 import ArticlePreview from './preview-templates/ArticlePreview';
 
+import CustomRelationControl from './widgets/customRelationWidget/CustomRelationControl';
+import CustomRelationPreview from './widgets/customRelationWidget/CustomRelationPreview';
+
 CMS.registerPreviewTemplate('categories', CategoryPreview);
 CMS.registerPreviewTemplate('articles', ArticlePreview);
+
+CMS.registerWidget('customRelation', CustomRelationControl, CustomRelationPreview);
