fa3f1e4fff631cb4e9a2115e7616fe135fa07eac
Carlos Gonzalez <homearanya@gmail.com>
2019-06-07 12:04:48 +0200

    Netlify CMS change slug to path

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,11 +5,13 @@ import ArticlePreview from './preview-templates/ArticlePreview';
 
 import CustomRelationControl from './widgets/customRelationWidget/CustomRelationControl';
 import CustomRelationPreview from './widgets/customRelationWidget/CustomRelationPreview';
-import CustomDateControl from './widgets/customDateWidget/CustomDateControl';
-import CustomDatePreview from './widgets/customDateWidget/CustomDatePreview';
-
+// import CustomDateControl from './widgets/customDateWidget/CustomDateControl';
+// import CustomDatePreview from './widgets/customDateWidget/CustomDatePreview';
+import PathControl from './widgets/pathWidget/PathControl';
+import PathPreview from './widgets/pathWidget/PathPreview';
 CMS.registerPreviewTemplate('categories', CategoryPreview);
 CMS.registerPreviewTemplate('articles', ArticlePreview);
 
 CMS.registerWidget('customRelation', CustomRelationControl, CustomRelationPreview);
-CMS.registerWidget('customDate', CustomDateControl, CustomDatePreview);
+// CMS.registerWidget('customDate', CustomDateControl, CustomDatePreview);
+CMS.registerWidget('path', PathControl, PathControl);
